e7581da621c000cbc1463ccf444fc5108efca669
BrunoFares <faresbruno04@gmail.com>
2023-11-19 23:51:04 +0200

    The code looks like a trainwreck but I don't care at this point, I may fix it later if I find it necessary

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const Gameboard = (function() {
     for (let i = 0; i < row; i++) {
         gameboard[i] = [];
         for (let j = 0; j < column; j++) {
-            gameboard[i][j] = 1;
+            gameboard[i][j] = "";
         }
     }
 
@@ -19,7 +19,7 @@ function inputToBoard(player, move) {
     if (player === 1) input = 'x';
     else if (player === 2) input = '0';
 
-    if (Gameboard[move[0]][move[1]] === 1) Gameboard[move[0]][move[1]] = input;
+    if (Gameboard[move[0]][move[1]] === "") Gameboard[move[0]][move[1]] = input;
     else console.log("This cell has already been played in.")
 
     return {Gameboard};
@@ -28,12 +28,56 @@ function inputToBoard(player, move) {
 const gridIsComplete = function() {
     for (let i = 0; i < 3; i++) {
         for (let j = 0; j < 3; j++) {
-            if (Gameboard[i][j] === 1) return false;
+            if (Gameboard[i][j] === "") return false;
         }
     }
     return true;
 }
 
 const Game = function() {
-    
+    let sign = "";
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 2; j++) {
+            if (Gameboard[i][j] === Gameboard[i][j+1]) sign = Gameboard[i][j];
+            else {
+                sign = "";
+                break;
+            }
+        }
+        if (sign === 'x' || sign === '0') return ("player win");
+    }
+
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 2; j++) {
+            if (Gameboard[j][i] === Gameboard[j+1][i]) sign = Gameboard[j][i];
+            else {
+                sign = "";
+                break;
+            }
+        }
+        if (sign === 'x' || sign === '0') return ("player win");
+    }
+
+    if (Gameboard[0][0] === Gameboard[1][1]) {
+        if (Gameboard[1][1] === Gameboard[2][2]) {
+            sign = Gameboard[0][0]; 
+            return 'player win';
+        }
+    }
+
+    if (Gameboard[0][2] === Gameboard[1][1]) {
+        if (Gameboard[1][1] === Gameboard[2][0]) {
+            sign = Gameboard[0][2]; 
+            return 'player win';
+        }
+    }
+}
+
+const gameReset = function() {
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            Gameboard[i][j] = '';
+        }
+    }
+    return Gameboard;
 }
