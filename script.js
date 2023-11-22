2c45a2a1c340207716af8efc4cc663dd3d61f40a
BrunoFares <faresbruno04@gmail.com>
2023-11-23 00:17:15 +0200

    My commits are worse than my code but I don't care, I finished this project

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 const div = document.querySelectorAll('div.gameboard div');
+const p = document.querySelectorAll('div.gameboard div p');
+const h2 = document.querySelector('h2');
+const button = document.querySelector('button');
+
+let playerTurn = 1;
 
 const Gameboard = (function() {
     const row = 3;
     const column = 3;
-    const gameboard = [];
+    let gameboard = [];
 
     for (let i = 0; i < row; i++) {
         gameboard[i] = [];
@@ -22,21 +27,23 @@ function inputToBoard(player, move) {
     else if (player === 2) input = 'o';
 
     if (Gameboard[move[0]][move[1]] === "") Gameboard[move[0]][move[1]] = input;
-    else console.log("This cell has already been played in.")
+    else return ("This cell has already been played in.")
 
-    return {Gameboard};
+    return { Gameboard, input };
 };
 
 const gridIsComplete = function() {
+    if (gameWin()) return true;
     for (let i = 0; i < 3; i++) {
         for (let j = 0; j < 3; j++) {
             if (Gameboard[i][j] === "") return false;
         }
     }
-    return true;
+    const draw = "It's a draw!";
+    return draw;
 }
 
-const gameWin = function() {
+function gameWin() {
     let sign = "";
     for (let i = 0; i < 3; i++) {
         for (let j = 0; j < 2; j++) {
@@ -46,7 +53,8 @@ const gameWin = function() {
                 break;
             }
         }
-        if (sign === 'x' || sign === 'o') return ("player win");
+        if (sign === 'x') return ('Player 1 wins!');
+        else if (sign === 'o') return ('Player 2 wins!');
     }
 
     for (let i = 0; i < 3; i++) {
@@ -57,46 +65,95 @@ const gameWin = function() {
                 break;
             }
         }
-        if (sign === 'x' || sign === 'o') return ("player win");
+        if (sign === 'x') return ('Player 1 wins!');
+        else if (sign === 'o') return ('Player 2 wins!');
     }
 
     if (Gameboard[0][0] === Gameboard[1][1]) {
         if (Gameboard[1][1] === Gameboard[2][2]) {
             sign = Gameboard[0][0]; 
-            return 'player win';
+            if (sign === 'x') return ('Player 1 wins!');
+            else if (sign === 'o') return ('Player 2 wins!');
         }
     }
 
     if (Gameboard[0][2] === Gameboard[1][1]) {
         if (Gameboard[1][1] === Gameboard[2][0]) {
             sign = Gameboard[0][2]; 
-            return 'player win';
+            if (sign === 'x') return ('Player 1 wins!');
+            else if (sign === 'o') return ('Player 2 wins!');
         }
     }
 }
 
-const gameReset = function() {
+function gameReset() {
     for (let i = 0; i < 3; i++) {
         for (let j = 0; j < 3; j++) {
             Gameboard[i][j] = '';
         }
     }
+    for (let k = 0; k < 9; k++) p[k].textContent = "";
+
+    h2.textContent = "-";
+
+    playerTurn = 1;
     return Gameboard;
 }
 
 for (let i = 0; i < div.length; i++) {
     div[i].addEventListener('click', () => {
-        let row = 2, column = 0;
+        let row, column;
 
-        while ((3 * row) + column != i) {
-            if (3 * row > i) {
-                row -= 1;
-                column += 1;
-            } else if (3 * row < i) {
-                column += 1;
-            }
+        switch(i) {
+            case 0:
+                row = 0, column = 0;
+                break;
+            case 1:
+                row = 0, column = 1;
+                break;
+            case 2:
+                row = 0, column = 2;
+                break;
+            case 3:
+                row = 1, column = 0;
+                break;
+            case 4:
+                row = 1, column = 1;
+                break;
+            case 5:
+                row = 1, column = 2;
+                break;
+            case 6:
+                row = 2, column = 0;
+                break;
+            case 7:
+                row = 2, column = 1;
+                break;
+            case 8:
+                row = 2, column = 2;
+                break;                                                                                                                        
+        }
+        
+        if (playerTurn === 1) {
+            inputToBoard(1, [row, column]);
+            playerTurn = 2;
+        }
+        else {
+            inputToBoard(2, [row, column]);
+            playerTurn = 1;
         }
 
-        inputToBoard(1, [row, column])
+        showInput(i, row, column);
+
+        if (gridIsComplete() === "It's a draw!") h2.textContent = gridIsComplete();
+        else if (gridIsComplete()) h2.textContent = gameWin();
     });
 }
+
+function showInput(i, row, column) {
+    p[i].textContent = Gameboard[row][column];
+}
+
+button.addEventListener('click', () => {
+    gameReset();
+});
